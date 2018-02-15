9d0d36bcbf0a6dfb4743e25448b3b0c2b9e38499
Jonatan_Mendoza <yonatanimm@gmail.com>
2018-02-14 20:47:51 -0600

    Adds main class and spring dependencies.

---
+++ src/main/java/com/javaconmanzanas/euler/EulerApplication.java
@@ -0,0 +1,12 @@
+package com.javaconmanzanas.euler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EulerApplication.class, args);
+	}
+}
